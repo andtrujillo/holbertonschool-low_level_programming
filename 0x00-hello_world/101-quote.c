8f3bdce00333ae6bf327ca68a50e9a31d384c205
Andres <1713@holbertonschool.com>
2020-02-05 21:20:39 +0000

    Added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main(void){
-	fprintf(stderr,"%s\n","and that piec of art is useful\" - Dora Korpar, 2015-10-19");
-	return 0;
+/**
+ * write - Function
+ * Return: integer
+ */
+int write(int filedes, const char *buf, unsigned int nbyte);
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 55);
+	return (0);
 }
