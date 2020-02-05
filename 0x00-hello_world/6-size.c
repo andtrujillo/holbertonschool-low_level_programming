c1b935f7f4174abe827ba830e98e7d35dde706a6
Andres <1713@holbertonschool.com>
2020-02-05 20:13:45 +0000

    Added the word byte to after the printf specifier

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %i(s)\n ", sizeof(char));
-	printf("Size of an int: %i(s)\n ", sizeof(int));
-	printf("Size of a long int: %d(s)", sizeof(long int));
-	printf("Size of a long long int: %d(s)\n", sizeof(long long int));
-	printf("Size of a float: %d(s)\n", sizeof(float));
+	printf("Size of a char: %i byte(s)\n ", sizeof(char));
+	printf("Size of an int: %i byte(s)\n ", sizeof(int));
+	printf("Size of a long int: %d bytes(s)", sizeof(long int));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %d bytes(s)\n", sizeof(float));
 	return (0);
 }
