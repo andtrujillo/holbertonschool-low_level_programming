678da2122ccd8042a353645dd6e486fd12385b69
Andres <1713@holbertonschool.com>
2020-02-05 20:01:39 +0000

    Added comments and removed trailing whitespaces increased function readability

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,13 @@
 /**
  * main - Entry point
  * Return: Always 0 (Success)
- * Prints 
  */
 int main(void)
 {
-        printf("Size of a char:%d\n ",sizeof(char));
-        printf("Size of an int: %i\n ", sizeof(int));
-        printf("Size of a long int: %l", sizeof(long int));
-        printf("Size of a long long int: %ll\n", sizeof(long long int));
-        printf("Size of a float: %lu\n",sizeof(float));
-        return (0);
+	printf("Size of a char: %d\n ", sizeof(char));
+	printf("Size of an int: %i\n ", sizeof(int));
+	printf("Size of a long int: %l", sizeof(long int));
+	printf("Size of a long long int: %ll\n", sizeof(long long int));
+	printf("Size of a float: %lu\n", sizeof(float));
+	return (0);
 }
