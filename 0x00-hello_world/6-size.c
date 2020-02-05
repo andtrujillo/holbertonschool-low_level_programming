a7693399af9530f6987f857bf5d7d15a852395a2
Andres <1713@holbertonschool.com>
2020-02-05 19:56:00 +0000

    Added script to output intel specific machine code assembly language from environment variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-int main(void){
-        printf("Size of a char:%c\n ",sizeof(char));
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Prints 
+ */
+int main(void)
+{
+        printf("Size of a char:%d\n ",sizeof(char));
         printf("Size of an int: %i\n ", sizeof(int));
         printf("Size of a long int: %l", sizeof(long int));
         printf("Size of a long long int: %ll\n", sizeof(long long int));
-        printf("Size of a float: %f\n",sizeof(float));
-        return 0;
+        printf("Size of a float: %lu\n",sizeof(float));
+        return (0);
 }
