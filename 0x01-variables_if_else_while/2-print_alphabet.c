bd6c345699f5a935b8e0223e5eb8f99ea26268ef
Andres <1713@holbertonschool.com>
2020-02-06 19:19:58 +0000

    Added 2-print_alphabet to repository

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 65; n < (65 + 26); n++)
+		putchar(n);
+	return (0);
+}
