b1dab36dbc285e5ab7bf4beb2181d68a5239f6b0
Andres <1713@holbertonschool.com>
2020-02-06 23:40:56 +0000

    Changed from two for loops to one loop incrementing the individual characters from decimal

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 /**
- * main - Entry Point
- * Return: Always 0 (Success)
- *
- */
+* main - Entry Point
+* Return: Always 0 (Success)
+*
+*/
 int main(void)
 {
-	int n, z;
+	int i, j, k;
 
-	for (n = 48; n < 58; n++)
+	for (i = 0; i < 100; i++)
 	{
-		for (z = 48; z < 58; z++)
+		j = i / 10;
+		k = i % 10;
+
+		putchar(j + '0');
+		putchar(k + '0');
+
+		if (i < 99)
 		{
-			putchar(n);
-			putchar(z);
-			if  (n != 57 || z != 57)
-				putchar(44);
+			putchar(44);
 			putchar(32);
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
