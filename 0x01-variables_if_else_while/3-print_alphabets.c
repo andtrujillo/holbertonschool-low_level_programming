e6c7fb908f421dd69f7bb8dd9c3f7d5af324ebb3
Andres <1713@holbertonschool.com>
2020-02-06 21:25:29 +0000

    Changed order of alphabet to reflect Holberton specifications

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int n;
 
-	for (n = 65; n < (65 + 26); n++)
+	for (n = 97; n < 123; n++)
 		putchar(n);
-	for (n = (65 + 26); n < ((65 + 26) + 26); n++)
+	for (n = 65 ; n < 91; n++)
 		putchar(n);
 	putchar('\n');
 	return 0;
