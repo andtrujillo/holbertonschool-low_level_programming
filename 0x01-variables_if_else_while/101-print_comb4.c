3fdc1dad3a0d52453ad8917d43beebe393752116
andtrujillo <60366425+andtrujillo@users.noreply.github.com>
2020-02-06 22:16:58 -0600

    Added advanced task 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, k, m;
+
+	n = '0';
+	while (n <= '7')
+	{
+		k = n + 1;
+		while (k <= '8')
+		{
+			m = k + 1;
+			while (m <= '9')
+			{
+				putchar(n);
+				putchar(k);
+				putchar(m);
+				if (n != '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				m++;
+			}
+			k++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
