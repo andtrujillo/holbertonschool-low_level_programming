03dbabda14eeabf64010731d0c462ea383e8bd5d
Andres Trujllo <1713@holbertonschool.com>
2020-02-11 08:51:50 -0600

    Removed return type zero on function print_alphabet and included function prototype

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include "holberton.h"
 /**
  * print_alphabet - Function that prints the alphabet in lowercase.
  *
@@ -13,5 +13,5 @@ void print_alphabet(void)
 	for (i = 97; i < (97 + 26); i++)
 		_putchar(i);
 	_putchar('\n');
-	return (0);
+
 }
