cf7a562ef011579dc0c4281b515783b219487ab5
Andres Trujllo <1713@holbertonschool.com>
2020-02-11 15:14:28 -0600

    Holberton School, 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,46 +1,41 @@
 #include "holberton.h"
-/**
- * print_to_98 - Function that prints all natural numbers from n to 98
- * @num: integer
- * Return: none
- */
+#include <stdio.h> 
+
 void print_to_98(int num_)
 {
-	int hasPrinted = 0, hasCall = 0;
-	
-	if (hasCall)
-	{
-		_putchar('0' + (num_ / 10));
-		_puchar('0' + (num_ % 10));
-		_putchar('\n');
-		hasCall = 1;
-	}
-	if (hasPrinted)
-	{
-		_putchar(',');
-		_putchar(' ');
-	}
-	
-	if (num_ > 98)
-	{
-		for (;num_ > 98; num_--)
-		{
-			if (num_ > 100)
-				_putchar( ((num_ /10)/10) + '0');
+	int i = 0, hasPrinted = 0;
 
-			_putchar( ((num_ /10) + '0');
-			_putchar( ((num_ % 10) + '0');
-		}
-	}else
+	if (num_ <= 98)
 	{
-		for (; num_ < 98; num_++)
+		
+		for(i = num_; i <= 98; i++)
 		{
-			_putchar( (num_ / 10) + '0');
-			_putchar( (num_ % 10) + '0');
-
+			if(!hasPrinted && num_ != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			printf("%d", i);
 		}
+		putchar('\n');
+		hasPrinted = 1;
 	}
-	hasPrinted = 1;
-	hasCall = 1;
+
+	hasPrinted = 0;
+	if (num_ >= 98) 
+	{
+		for (i = num_; i >= 98; i--)
+		{
+			if(!hasPrinted && num_ != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+			printf("%d",i);
+		}
+		if(hasPrinted)
+			_putchar('\n');
+		hasPrinted = 1;
+	}
 }
-
