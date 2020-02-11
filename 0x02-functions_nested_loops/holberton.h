8a6035a9745cf0d97e53e4e596c7ec9ab90a2217
Andres Trujllo <1713@holbertonschool.com>
2020-02-11 11:20:38 -0600

    Added all function prototypes to holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+void print_times_table(int n);
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
 int print_last_digit(int);
 int _abs(int);
 int print_sign(int n);
