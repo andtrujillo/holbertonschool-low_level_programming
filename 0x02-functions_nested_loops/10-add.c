272277e63958eccf703ba06161410e51d36491c0
Andres Trujllo <1713@holbertonschool.com>
2020-02-11 12:33:13 -0600

    Removed spaces from header and comments

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,8 @@
 #include "holberton.h"
-
 /**
  * add - Function that adds two integers and returns the result
  * Return: integer
  */
-
 int add(int fir_num, int sec_num)
 {
 	int result = fir_num + sec_num;
