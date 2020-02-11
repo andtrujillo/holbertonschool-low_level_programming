894dbea1d7486f232e587f0f43b11ab27e9477c1
Andres Trujllo <1713@holbertonschool.com>
2020-02-11 12:28:40 -0600

    Removed whitespace and put space after declarations

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,12 @@
 
 /**
  * add - Function that adds two integers and returns the result
- * Return: integer 
+ * Return: integer
  */
 
 int add(int fir_num, int sec_num)
 {
 	int result = fir_num + sec_num;
+
 	return (result);
 }
