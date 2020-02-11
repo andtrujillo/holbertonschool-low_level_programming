3d1939d9289855154e852211b203d531b71f9d9c
Andres Trujllo <1713@holbertonschool.com>
2020-02-11 10:18:27 -0600

    Changed 3 if statements to if, else if and else statement and added function prototype to holberton.h

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,26 @@
+#include "holberton.h"
+
 /**
  * print_sign - Function that prints the sign of a number
  * @n: integer parameter of number to be checked
- * Returns: 
+ * Returns:
  *	1 and prints '+' if 'n' is greater than zero
  *	0 and prints '0' if 'n' is zero
  *	-1 and prints '-' if 'n' is less than zero
  */
 int print_sign(int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
 	if (n > 0)
 	{
 		_putchar('+');
+		return (0);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
 		return (1);
 	}
-	if (n < 0)
+	else
 	{
 		_putchar('-');
 		return (-1);
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,8 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
+int print_sign(int n);
 int _isalpha(int c);
 int _islower(int c);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+
 #endif
