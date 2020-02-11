0f294eaef683c437786a39e56583eb9173adbd26
Andres Trujllo <1713@holbertonschool.com>
2020-02-11 12:58:37 -0600

    Fixed variable logic

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,19 @@
 #include "holberton.h"
 /**
  * print_last_digit - Function that prints the last digit of a number
+ * @last: number to take last digit from
  * Return: The value of the last digit
  */
 int print_last_digit(int last)
 {
-	unsigned int temp = 0;
+	int temp;
 
 	temp = last % 10;
+
 	if (temp < 0)
-		temp *= -1;
+		temp = temp * -1;
+
 	_putchar('0' + temp);
+	
 	return (temp);
 }
