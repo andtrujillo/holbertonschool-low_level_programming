712746f0043554a9dc633a6759c9b14e7b1dde80
Andres Trujllo <1713@holbertonschool.com>
2020-02-11 10:57:47 -0600

    Added functionality for negative numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,15 @@
 #include "holberton.h"
-
 /**
  * print_last_digit - Function that prints the last digit of a number
  * Return: The value of the last digit
  */
 int print_last_digit(int last)
 {
-	last = last % 10;
-	_putchar(last);
-	return (last
+	int temp = 0;
+
+	if (last < 0)
+		last *= -1;
+	temp = last % 10;
+	_putchar('0' + temp);
+	return (temp );
 }
