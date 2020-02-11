6e7294a0dc43d5dd37a78243b45b587a95968e01
Andres Trujillo <aft@crunchybangbang.b1>
2020-02-11 17:41:34 -0600

    Added proper return comment instead of copying and pasting specification sheet

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,10 +3,7 @@
 /**
  * print_sign - Function that prints the sign of a number
  * @n: integer parameter of number to be checked
- * Returns:
- *	1 and prints '+' if 'n' is greater than zero
- *	0 and prints '0' if 'n' is zero
- *	-1 and prints '-' if 'n' is less than zero
+ * Return: integer
  */
 int print_sign(int n)
 {
