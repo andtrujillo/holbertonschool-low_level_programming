b223547e93363baec944747824f8f9311297aa1d
andres <aft@cpbb.b1>
2020-02-11 08:40:51 -0600

    Added header file and included header in 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include "holberton.h"
+#include "_putchar.c"
+
 /**
  * main - Entry point to 0-holberton.c
  * Description: Program that prints Holberton and is followed by a newline
