3756df80d7c07851d767ca5e9b57aec7b2cd61a2
Andres Trujllo <1713@holbertonschool.com>
2020-02-11 12:34:42 -0600

    Added descriptions to parameters to function

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,8 @@
 #include "holberton.h"
 /**
  * add - Function that adds two integers and returns the result
+ * @fir_num: first number to add 
+ * @sec_num: second number to add with first
  * Return: integer
  */
 int add(int fir_num, int sec_num)
