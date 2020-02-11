1d1b0dc5310700ccd2fde7fe39f0a00e790ad4b5
Andres Trujllo <1713@holbertonschool.com>
2020-02-11 15:47:01 -0600

    Betty linted file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@ void print_to_98(int num_)
 {
 	int i = 0;
 
-	for(i = num_; i <= 98; i++)
+	for (i = num_; i <= 98; i++)
 	{
 		printf("%d", i);
 		if (i != 98)
@@ -12,15 +12,12 @@ void print_to_98(int num_)
 			putchar(',');
 			putchar(' ');
 		}
-
 	}
 	for (i = num_; i >= 98; i--)
 	{
 		if (num_ == 98)
 			break;
-
-		printf("%d",i);
-
+		printf("%d", i);
 		if (i != 98)
 		{
 			putchar(',');
