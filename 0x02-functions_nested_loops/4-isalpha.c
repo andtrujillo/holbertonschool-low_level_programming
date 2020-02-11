cf5c70adf624fb0a81725c347f09cc1f09543f17
Andres Trujllo <1713@holbertonschool.com>
2020-02-11 09:41:02 -0600

    Changed logic from nested if statement to seperate if statements

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,13 +6,14 @@
  */
 int _isalpha(int c)
 {
-	if (c < 123 && c > 64)
+	if (c < 123 && c > 96)
 	{
-		if (c > 96 && c < 91)
 			return (1);
 	}
-	
+	if (c > 64  && c < 91)
+	{
+		return (1);
+	}
+
 	return (0);
-
-
 }
