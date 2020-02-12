80749adaf297b2fc448f14c73174f7d33e5f75a2
Andres Trujillo <aft@crunchybangbang.b1>
2020-02-11 21:53:03 -0600

    Made sure I was printing within bounds of specifications

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- * main - Entry point to our program that prints the first 50 Fibonacci numbers,
+ * main - Our program that prints the first 50 Fibonacci numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	long int nextFib = 0, firstFib = 0, secondFib = 1;
 	int i;
-	
+
 	for (i = 0; i < 50; i++)
 	{
-		nextFib = firstFib + secondFib;	
+		nextFib = firstFib + secondFib;
 		firstFib = secondFib;
 		secondFib = nextFib;
 
@@ -18,8 +18,8 @@ int main(void)
 			printf("%ld, ", nextFib);
 		else
 			printf("%ld\n", nextFib);
-		
-		
+
+
 	}
 	return (0);
 }
