4aabaf8964236c35b8bd4ef1b8dd7a9b928f91b6
Andres Trujillo <aft@crunchybangbang.b1>
2020-02-11 18:41:25 -0600

    Updated integer type to long

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point to our program that prints the first 50 Fibonacci numbers,
- * starting with 1 and 2, followed by a new line.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int 1, nextFib = 0, firstFib = 1, secondFib = 2;
+	long 1, nextFib = 0, firstFib = 1, secondFib = 2;
 	
 	printf("%d, %d ", firstFib, secondFib);
-	for (i = 0; i < 49; i++)
+	for (i = 0; i < 47; i++)
 	{
 
 		nextFib = firstFib + secondFib;
