3ae577b4c5e429e28bb0d628bfb37e4fff38c149
Andres Trujillo <aft@crunchybangbang.b1>
2020-02-11 18:12:52 -0600

    Corrected output with printf flags and modifiers

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,25 +1,26 @@
 #include<stdio.h>
 /**
- * main - Entry point of function that prints the times table of n
+ * print_times_table- Entry point of function that prints the times table of n
  * @n: The times table to be printed
  * Return: void (none)
  */
-void print_times_table(void)
+void print_times_table(int n)
 {
 	int i, j;
-	if (n < 16)
+
+	if (n <= 0 && n >= 15)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for(j = 0; j < n; j++)
+			printf("0,");
+			for (j = 0; j <= n; j++)
 			{
-				if( (n-1) == i)
-					printf("%d",i*j);
+				if (j == n)
+					printf("%3d", (i * j));
 				else
-					printf("%d, ", i*j);
+					printf("%3d, ", (i * j));
 			}
 			putchar('\n');
 		}
 	}
-	return (0);
 }
