1eeda4a3581633eccd4bf7af24f084bdd5f69973
andtrujillo <60366425+andtrujillo@users.noreply.github.com>
2020-02-12 03:16:50 -0500

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	long n = 0, max = 4000000, firstFib = 1, secondFib = 2;
+	int k = 0, firstFib = 1, secondFib = 2, sum = 0;
 
-	while (firstFib < max && secondFib < max)
+	while (secondFib < 4000000)
 	{
-		if ((n % 2) == 0)
-			n += n;
-		n = firstFib + secondFib;
-		firstFib = secondFib;
-		secondFib = n;
+		if (secondFib % 2 == 0)
+			sum += secondFib;
+		k = secondFib;
+		secondFib += firstFib;
+		firstFib = k;
 	}
-	printf("%ld\n", n);
+	printf("%d\n", sum);
 	return (0);
 }
