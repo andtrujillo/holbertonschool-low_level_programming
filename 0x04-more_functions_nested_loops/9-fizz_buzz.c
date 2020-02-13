3e101a8049a2162a4371bee418738aae10b39fa9
Andres Trujillo <andtruj23@gmail.com>
2020-02-13 16:57:07 -0600

    Added range to include one-hundred and exclude zero

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,17 +8,21 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
 		{
-			print("Fizz");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		else 
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
 		{
 			printf("%d", i);
 		}
