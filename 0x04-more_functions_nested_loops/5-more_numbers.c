213649c4855b65ccd8cd5976878dda99354afec2
Andres Trujllo <1713@holbertonschool.com>
2020-02-13 14:44:16 -0600

    Removed comments and shortened code with peer help

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,27 +5,18 @@
  */
 void more_numbers(void)
 {
-	int i, j, temp_1, temp_2;
+	int i, j;
 
-	for (i = 0; i < 10;i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0;j <= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
-			temp_1 = j;
-			temp_2 = i;
-
-			if (temp_1 > 9)
-			{	
-				temp_1 = j / 10;
-				temp_2 = j % 10;
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
 			}
-				_putchar(temp_1 + '0');	
+			_putchar((j % 10) + '0');
 		}
-			_putchar(temp_2 + '0'); /* because 10s place only goes to ones place so therefore the first  */
-															/* number to print over and over first ten times can be i */
-														  /* then I print both j at 10s place */
-
-		}
-	}
 	_putchar('\n');
+	}
 }
