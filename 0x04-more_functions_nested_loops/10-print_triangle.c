0acf1584b592a0d3ed4aa1cc1cdc0e90601cba0b
Andres Trujillo <andtruj23@gmail.com>
2020-02-13 18:13:05 -0600

    Fixed trailing whitespace

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,18 +9,15 @@ void print_triangle(int size)
 	int i, j, k;
 
 	if (size < 1)
-	{
 		_putchar('\n');
-	}
 
 	for (i = 1; i <= size; i++)
 	{
 		for (j = size + 1; j > i; j--)
-		{	_putchar(' ');}
-		for (k = 0; k < i; k++)
-		{	_putchar(35);}
-		
-		_putchar('\n');
+		{	_putchar(' '); }
 
+		for (k = 0; k < i; k++)
+		{	_putchar(35); }
+		_putchar('\n');
 	}
 }
