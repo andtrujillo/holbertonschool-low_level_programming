e007fb3ed820f4d396e884a8e9e4b4a926907bb7
andtrujillo <60366425+andtrujillo@users.noreply.github.com>
2020-02-18 09:35:09 -0600

    Function that prints last half of array

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * puts_half - prints last half of array
+ * @str: character array to be printed
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int length, characters = 0;
+
+	while (*(str + length) != '\0')
+		length++;
+
+	if (length % 2 != 0)
+		characters = (length - 1) / 2;
+	else
+		characters = length / 2;
+	characters++;
+
+	while (characters < length)
+		_putchar(*(str + characters++));
+
+	_putchar('\n');
+}
