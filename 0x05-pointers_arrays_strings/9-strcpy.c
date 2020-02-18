8472332569b4ff762c502b022e84b993e625b8e0
Andres Trujillo <andtruj23@gmail.com>
2020-02-18 05:10:31 -0600

    Created function that copies a string to destination pass by reference

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,17 +3,18 @@
  * _strcpy - copy destination string from source string
  * @dest: char array to be copied to.
  * @src: char array to be copied from.
- * Return: pointer of type char 
+ * Return: pointer of type char
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 	char *save = dest;
+
 	while (src[i] != '\0')
 	{
-		dest[i] = src[i]; 
+		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return (save);		
+	return (save);
 }
