f82e4ab85e4ea3555eebd111cc59285e5a1d9943
Andres Trujllo <1713@holbertonschool.com>
2020-02-18 12:05:49 -0600

    Updated reverse string function

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * rev_string - reverses a string.
- * @string: character string array to be flipped via pointer
+ * @s: character string array to be flipped via pointer
  * Return: 0
  */
 void rev_string(char *s)
@@ -10,11 +10,11 @@ void rev_string(char *s)
 	char rev;
 
 	while (*copy)
-		copy++; 	
+		copy++;
 	copy = copy - 1;
 	while (s < copy)
 	{
-		rev = *s;	
+		rev = *s;
 		*s++ = *copy;
 		*copy-- = rev;
 	}
