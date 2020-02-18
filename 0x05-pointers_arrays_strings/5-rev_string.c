09c3ec1ab73209cf82bb7dbabc8e932b2fc216e4
Andres Trujillo <andtruj23@gmail.com>
2020-02-18 02:34:31 -0600

    Fixed logic with help from peer to meet Holberton standards

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,26 +6,24 @@
  */
 void rev_string(char *string)
 {
-	char rev_string[_strlen(string)];
-	int i, j;
+	int i = 0;
+	int	j = 0;
+	int	k;
+	int	rev_string[90];
 
-	for (i = _strlen(string) - 1, j = 0; i >= 0; i--, j++)
-		rev_string[j] = string[i];
-	int size = sizeof(rev_string);
-
-	for (i = 0; i < size; i++)
-		string[i] = rev_string[i];
-}
-/**
- * _strlen - Function that counts the characters in array
- * @src: pointer to char array
- * Return: integer
- */
-int _strlen(char *src)
-{
-	int length = 0;
-
-	while (*src++)
-		length++;
-	return (length);
+	while (string[i] != '\0')
+		i++;
+	k = i;
+	while ((i != 0) && (string[j] != '\0'))
+	{
+		rev_string[i] = string[j];
+		j++;
+		i--;
+	}
+	i = 0;
+	while ((rev_string[i] != '\0') && (i < k))
+	{
+		string[i] = rev_string[i + 1];
+		i++;
+	}
 }
