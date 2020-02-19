1af60eaf508ab90d72e83d1efcd31ff94b63d53c
Andres Trujllo <1713@holbertonschool.com>
2020-02-18 20:36:24 -0600

    Added advanced task without Fletcher checksum

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+
+	srand(time(0));
+
+	char *alpha_nums = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghipqrstuvwxyz0123456789";
+	char arr[2048];
+	int sum = 0;
+	int i = 0;
+	int num = 0;
+	int k = 0;
+
+while (sum <  2772 - 'z')
+{
+	num = rand() % 62;
+	sum += alpha_nums[num];
+	arr[i] = alpha_nums[num];
+	i++;
+}
+	k = 2772 - sum;
+	arr[i] = k;
+
+	printf("%s\n", arr);
+	return (0);
+}
