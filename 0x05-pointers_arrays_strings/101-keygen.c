6177c1e43d59ffe5aa0d3c5070d9e9a288f39380
Andres Trujllo <1713@holbertonschool.com>
2020-02-18 20:47:23 -0600

    Added advanced task without Fletcher checksum

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+/**
+* main - Entry point for function that cracks file
+* Return: 0 (Success)
+*/
 int main(void)
 {
-
-	srand(time(0));
-
-	char alpha_nums[62] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghipqrstuvwxyz0123456789";
-	char arr[2048];
+	char alpha_nums[62]
+		=	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghipqrstuvwxyz0123456789";
+	char arr[2048] = 0;
 	int sum = 0, i = 0, num = 0, k = 0;
 
-while (sum <  2772 - 'z')
-{
-	num = rand() % 62;
-	sum += alpha_nums[num];
-	arr[i] = alpha_nums[num];
-	i++;
-}
+	srand(time(0));
+	while (sum < 2772 - 'z')
+	{
+		num = rand() % 62;
+		sum += alpha_nums[num];
+		arr[i] = alpha_nums[num];
+		i++;
+	}
 	k = 2772 - sum;
 	arr[i] = k;
 
