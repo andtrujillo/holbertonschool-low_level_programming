70cdb064d6007fe7d3ac6003e53148518fc72d24
andtrujillo <60366425+andtrujillo@users.noreply.github.com>
2020-02-20 12:20:33 -0600

    Updating with rest of prototype functions

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -3,11 +3,18 @@
 
 /* int */
 int _strlen(char *str);
+int _strcmp(char *s1, char *s2);
 
 /* char */
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
+char *cap_string(char *);
+char *string_toupper(char *);
+char *rot13(char *);
+char *leet(char *);
 
 /* void */
-
+void reverse_array(int *a, int n);
 
 #endif
