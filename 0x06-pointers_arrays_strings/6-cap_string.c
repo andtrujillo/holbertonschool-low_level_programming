b3a035d61e6ba12162095aedcc2a21070799ae83
Andres Trujllo <1713@holbertonschool.com>
2020-02-20 15:53:31 -0600

    Added string to changes first character of word to capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * cap_string - function that capitalizes all lowercase characters
+ * @s: lowercase string to be capitalized
+ * Return: character array of strings capitalized from lowercase
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i + 1] == '\0')
+			break;
+
+		if ((s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' ||
+					s[i] == '?' || s[i] == '\"' || s[i] == '(' || s[i] == ')' ||
+					s[i] == '{' || s[i] == '}' || s[i] == ' ' || s[i] == '\n' ||
+					s[i] == '\t')  && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		{
+			s[i + 1] -= 32;
+		}
+	}
+	return (s);
+}
