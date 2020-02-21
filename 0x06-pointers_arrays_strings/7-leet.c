c1fcb4ae69043397e5526bd4d33415ed548527e9
Andres Trujllo <1713@holbertonschool.com>
2020-02-20 19:46:18 -0600

    Removed strlen function

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,30 +7,17 @@
 char *leet(char *un_leet)
 {
 	int i, j;
-	int leet_length = _strlen(un_leet);
-	char *characters = "aAeEoOtTlL";
-	char *leet = "4433007711";
+	char characters[] = "aAeEoOtTlL";
+	char leet[] = "4433007711";
 
-	for (i = 0; un_leet[i] < leet_length; i++)
+	for (i = 0; un_leet[i] != '\0'; i++)
 	{
-		for (j = 0; j < _strlen(characters); j++)
+		for (j = 0; j <= 9 ; j++)
 		{
-			if (characters[j] == un_leet[i])
-				un_leet[i] = leet[j];
+			if (un_leet[i] == characters[j])
+				un_leet[i] == leet[j];
+
 		}
 	}
 	return (un_leet);
-}
-/**
- * _strlen - function that counts length of string
- * @s: string to determine length
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	int l = 0;
-
-	while (*s++)
-		l++;
-	return (l);
 }
