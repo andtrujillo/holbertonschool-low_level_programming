f5510781a54784e04ae6995bb93194999ee965b8
Andres Trujllo <1713@holbertonschool.com>
2020-02-20 18:48:00 -0600

    Adding leet speak

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * leet - translating string to leet speak
+ * @un_leet: character array to be converted to 1337
+ * Return: string conversion to 1337
+ */
+char *leet(char *un_leet)
+{
+	int i, j;
+	int leet_length = _strlen(un_leet);
+	char *characters = "aAeEoOtTlL";
+	char *leet = "4433007711";
+
+	for (i = 0; un_leet[i] < leet_length; i++)
+	{
+		for (j = 0; j < _strlen(characters); j++)
+		{
+			if (characters[i] == un_leet[j])
+				un_leet[j] = leet[i];
+		}
+	}
+	return (un_leet);
+}
+/**
+ * _strlen - function that counts length of string
+ * @s: string to determine length
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s++)
+		l++;
+	return (l);
+}
