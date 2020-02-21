4872b48337f033f72ebad2fd9bae87db69a133d5
Andres Trujllo <1713@holbertonschool.com>
2020-02-20 19:47:50 -0600

    Adding advanced task

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_number - print num_ with putchar
+ * @num_: number to print
+ * Return: always void
+ */
+void print_number(int num_)
+{
+	if (num_ < 0)
+	{
+		_putchar('-');
+		print_one_digit(-(unsigned int)num_);
+	}
+	else
+		print_one_digit(num_);
+
+}
+/**
+ * print_one_digit - prints an unsigned integer
+ * @num_: an unsigned integer to print
+ * Return: always void
+ */
+void print_one_digit(unsigned int num_)
+{
+	if (num_ / 10 != 0)
+	{
+		print_one_digit(num_ / 10);
+	}
+	_putchar((num_ % 10) + '0');
+}
