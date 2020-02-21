e5b1ceb812e4e8aacb7c8315878813939ee4897d
Andres Trujllo <1713@holbertonschool.com>
2020-02-20 19:40:00 -0600

    Added logic to make it print properly

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -32,19 +32,8 @@ char *rot13(char *rot_str)
 int _strlen(char *s)
 {
 	int l = 0;
+
 	while (*s++)
 		l++;
 	return (l);
-}
-char *string_toupper(char *s)
-{
-	int i;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-		{
-			s[i] -= 32;
-		}
-	}
-		return (s);
 }
