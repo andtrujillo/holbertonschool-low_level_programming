412fe2e71f7298248ab9ed65672da9722fbd7057
Andres Trujllo <1713@holbertonschool.com>
2020-02-20 21:53:48 -0600

    Function that uses pointer to change value of variable not originally pointing to it

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -1,31 +1,24 @@
-#include "holberton.h"
+#include <stdio.h>
 
-/**
- * print_number - print num_ with putchar
- * @num_: number to print
- * Return: always void
- */
-void print_number(int num_)
+int main(void)
 {
-	if (num_ < 0)
-	{
-		_putchar('-');
-		print_one_digit(-(unsigned int)num_);
-	}
-	else
-		print_one_digit(num_);
+  int n;
+  int a[5];
+  int *p;
 
-}
-/**
- * print_one_digit - prints an unsigned integer
- * @num_: an unsigned integer to print
- * Return: always void
- */
-void print_one_digit(unsigned int num_)
-{
-	if (num_ / 10 != 0)
-	{
-		print_one_digit(num_ / 10);
-	}
-	_putchar((num_ % 10) + '0');
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5)= 98;
+  /* ...so that this prints 98\n */
+
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
