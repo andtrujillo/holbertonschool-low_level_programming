49638c51d56d01e2982c30276b6eb0ba1e70a5a2
andtrujillo <60366425+andtrujillo@users.noreply.github.com>
2020-02-21 23:12:51 -0600

    Update 102-infinite_add.c

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -5,87 +5,57 @@
  * @second_num: Second number to add to first number
  * Return: zero, on failure otherwise returns character array
  */
- char *infinite_add(char *first_num, char *second_num, char *buffer, int size)
- {
-  int i;
-  int result = 0;
-  int carry = 0;
-  /* Determining the length of 
-   * our two passed arrays
-   * and the size of the buffer.
-   */
-  int first_num_length = _strlen(first_num);
-  int second_num_length = _strlen(second_num);
-  int bufsize = _strlen(buffer);
-  
-  /* The result won't fit in passed function 
-   * so return zero 
-   */
-  if (bufsize > size)
-    return (0);
-    
-  /* Parse our data's index to find what values can
-   * be carried over, since these numbers plus
-   * the second_num is its own value because there
-   * are no more numbers in the difference of arrays.
-   */
-  index = _abs(second_num_length - first_num_length);
-  /* Fill the second number with zeros  
-   * instead of null characters.
-   */
-  if (second_num_length > first_num_length)
-  {
-    for (i = 0; i < index; i++)
-    {
-      first_num[i] = 0;
-    }
-  }
-  /* Fill the first number with zeros
-   * instead of null characters.
-   */
-  if (first_num_length > second_num_length)
-  {
-    for (i = 0; i < index; i++)
-    {
-      second_num[i] = 0;
-    }
-  }
-  if (first_num_length >= second_num_length)
-  {
-    for (i = first_num_length; i > index; i--)
-    {
-      result = first_num[i] + second_num[i] + carry;
-    
-      if (result >= 10)
-      {
-       carry = result / 10;
-       buffer[i] = result % 10;
-      }
-      else
-      {
-        buffer[i] = result;
-      }
-    }
-  }
-  if (second_num_length >= first_num_length)
-  {
-    for (i = second_num_length; i > index; i--)
-    {
-      result = first_num[i] + second_num[i] + carry;
-      
-      if (result >= 10)
-      {
-        carry = result / 10;
-        buffer[i] = result % 10;
-      }
-      else
-      {
-        buffer[i] = result;
-      }
-    }
-  }
- }
- /**
+char *infinite_add(char *first, char *second, char *r, int size)
+{
+	int i;
+	int result = 0;
+	int carry = 0;
+	
+	int f_l = _strlen(first);
+	int s_l = _strlen(second);
+	int b_sz = _strlen(r);
+	int index1 = _abs(f_l-s_l);
+	for (i = f_l; i > f_l; i--)
+	{
+		result = (first[i] - '0') + (second[i] - '0') + carry;
+		if (result >= 10)
+		{
+			carry = result / 10;
+			r[i] = result % 10;
+			
+			if (i == index1)
+				break;
+		}
+		else
+		{
+			r[i] = result;
+		}
+	}
+	while(i != 0)
+	{
+		r[i] = 0 + (second[i] - '0');
+		i--;
+	}
+	return (r);
+}
+int _abs(int n)
+{
+	int p = 1;
+	
+	if (n < 0)
+		p *= -1;
+	else
+		p *= 1;
+	return (p);
+		
+}
+int _strlen(char *n)
+{
+	int length = 0;
+	while(*n++)
+		length++;
+	return (length);
+} /**
   * _abs - Function that returns absolute value of integer
   * @n: Number passed to function to return the absolute 
   * Return: integer always (Success)
