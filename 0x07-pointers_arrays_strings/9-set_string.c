72c6da594ca497f9a3107fb7e606e0cc3ec422ac
Andres Trujillo <1713@holbertonschool.com>
2020-02-24 22:24:21 +0000

    Function that sets the value of a pointer to a char *

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -7,27 +7,5 @@
  */
 void set_string(char **s, char *to)
 {
-	printf("%p\n",**s);
-	printf("%p\n", &s);
-	printf("%s\n", *s);
-	printf("%s\n", *to);
- **s = *to;
-//*s = _memset(s, to, _strlen(to));	
+	*s = to;
 }
-int _strlen(char *s)
-{
-	int length = 0;
-	while(*s++)
-		length++;
-	return (length);
-}
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *dest = s;
-
-	while (n-- > 0)
-		*dest++ = b;
-
-	return (s);
-}
-
