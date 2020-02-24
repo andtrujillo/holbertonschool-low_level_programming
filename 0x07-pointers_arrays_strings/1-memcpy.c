d07770aa3fac9b8407a8f92d69f24e76861607b0
Andres Trujillo <1713@holbertonschool.com>
2020-02-24 21:42:32 +0000

    Linted and added description for function parameter

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,14 +4,15 @@
  * _memcpy - function that copies memory
  * @dest: destination string
  * @src: source string from which to copy from
+ * @n: number of bytes to copy
  * Return: character array
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  char *d = dest;
-  const char *s = src;
- 
-  while (n--)
-    *d++ = *s++;
-  return dest;
+	char *d = dest;
+	const char *s = src;
+
+	while (n--)
+		*d++ = *s++;
+	return (dest);
 }
