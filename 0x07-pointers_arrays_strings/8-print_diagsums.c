61a3495b4dd89c4a3629815efe9c37b6d273c52b
Andres Trujillo <1713@holbertonschool.com>
2020-02-25 01:44:03 +0000

    Refactored code for row-major array functionality

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,20 +1,25 @@
 #include "holberton.h"
-/* print_diagsums - function that prints the sum of two diagonals of a square matrix of integers
+/**
+ * print_diagsums - function that prints the
+ * sum of two diagonals of a square matrix of integers
  * @a: array of integers
  * @size: size of array of integers
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum_left, sum1;
+	int i, j, k, sum = 0, sum1 = 0;
+
 	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size; j++)
-		{
-			if (i == j)
-				sum_left += *(a + i * size + j);
-			else if ((i + j) == (size - 1))
-				sum1 += *(a + i * size + j);
-		}
+		k = (i * size) + 1;
+		sum += *(a + k);
 	}
-	printf("%d, %d\n", sum_left, sum1);
+	k = 0;
+	for (j = 0; j < size; j++)
+	{
+		k = (j * size) + (size - 1 - j);
+		sum1 += *(a + k);
+	}
+
+	printf("%d, %d\n", sum1, sum);
 }
