908dfd2fba140f389340826814a16d6b5e2f2c31
Andres Trujillo <1713@holbertonschool.com>
2020-02-25 02:35:59 +0000

    Refactored code to use one subscripted value to access array

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,19 +7,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, k, sum = 0, sum1 = 0;
+	int i, n, total1 = 0, total2 = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		k = (i * size) + 1;
-		sum += *(a + k);
-	}
-	k = 0;
-	for (j = 0; j < size; j++)
-	{
-		k = (j * size) + (size - 1 - j);
-		sum1 += *(a + k);
-	}
+	for (i = 0; i <= (size * size); i = i + size + 1)
+			total1 = total1 + a[i];
 
-	printf("%d, %d\n", sum, sum1);
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+			total2 = total2 + a[n];
+	printf("%d, %d\n", total1, total2);	
 }
