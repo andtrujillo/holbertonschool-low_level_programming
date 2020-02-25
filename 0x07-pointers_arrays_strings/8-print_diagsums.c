6dbe238b5b4832a35f6e8d1d2bc982a2c6a39ea2
Andres Trujillo <1713@holbertonschool.com>
2020-02-25 02:45:07 +0000

    Fixed which numbers print first from sum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,10 +8,10 @@
 void print_diagsums(int *a, int size)
 {
 	int i, k, sum1 = 0, sum2 = 0;
-	
+
 	for (i = 0; i <= (size * size); i = i + size + 1)
-			sum1 += a[i];
+		sum1 += a[i];
 	for (k = size - 1; k <= (size * size) - size; k = k + size - 1)
-			sum2 += a[k];
+		sum2 += a[k];
 	printf("%d, %d\n", sum1, sum2);
 }
