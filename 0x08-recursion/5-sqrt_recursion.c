40bfceb9f2294e9bb5ccf10db89b777a398636fc
Andres <1713@holbertonschool.com>
2020-02-27 00:30:18 +0000

    Function for finding square of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,27 @@
 #include "holberton.h"
-/** 
+int _sqrt(int i, int n);
+/**
  * _sqrt_recursion - function that returns the natural square root of a number
  * @n: number to find square of
  * Return: integer value
  */
 int _sqrt_recursion(int n)
 {
-	if ( n % 2 == 0 )
-	{
-		
-	}
+	int x;
+
+	x = _sqrt(0, n);
+	return (x);
 }
+int _sqrt(int i, int n)
+{
+	int x;
+
+	if ((i * i) == n)
+		x = i;
+	if ((i * i) < n)
+		x = _sqrt(i + 1, n);
+	if ((i * i) > n)
+		x = -1;
+	return (x);
+}
+
