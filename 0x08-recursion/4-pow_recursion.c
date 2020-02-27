f209ef602a528990c4e523c43ba8ad61537c148d
Andres Trujillo <1713@holbertonschool.com>
2020-02-27 04:24:33 +0000

    Adjusted spacing from pushing from github mobile

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,18 +1,19 @@
 #include "holberton.h"
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y
- * @x: the base number 
+ * _pow_recursion - function that returns the value of x
+ * raised to the power of y
+ * @x: the base number
  * @y: the exponent
  * Return: integer
  */
 int _pow_recursion(int x, int y)
 {
-        if (y < 0)
-                return(-1);
-        if (y == 0)
-                return (1);
-        if (y > 0)
-                return(_pow_recursion(x, y - 1)) * x;
-        return(0);
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+if (y > 0)
+return (_pow_recursion(x, y - 1)) * x
+return (0);
 }
 
