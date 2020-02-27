2da09d55da73c38e2359c6977db38381d18db0f6
Andres <1713@holbertonschool.com>
2020-02-27 01:29:51 +0000

    Adding function that returns 1 when it finds a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,36 @@
 #include "holberton.h"
+int prime(int x, int n);
 /**
- * is_prime_number - function that returns 1 if the input integer is a prime number
+ * is_prime_number - function that returns 1
+ * if the input integer is a prime number
  * otherwise return 0
- * #include "holberton.h"
- /**
-* is_prime_number - function that returns 1 if the input integer is a prime number
-* otherwise return 0
-* #include "holberton.h"
-/*** is_prime_number - function that returns 1 if the input integer is a prime number
-* otherwise return 0
-* 
+ * @n: test condition for is a prime
+ * Return: boolean integer
+ */
+int is_prime_number(int n)
+{
+	return (prime(3, n));
+}
+/**
+ * prime - function that returns whether a number is prime
+ * @x: number from where to start from
+ * @n: number being checked containing all properties of a prime number
+ * Return: integer
+ */
+int prime(int x, int n)
+{
+	if (n <= 1)
+		return (0);
+	if (n <= 3)
+		return (1);
+	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
+		return (0);
+	if ((x * x) <= n)
+	{
+		if (n % x != 0 || n % (x + 2) != 0)
+			prime(x + 6, n);
+		else if (n % x == 0 || n % (x + 2) == 0)
+			return (0);
+	}
+	return (1);
+}
