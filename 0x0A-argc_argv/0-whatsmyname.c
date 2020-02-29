d8006f9d59a2a603c64d20c91d532d1ba3c28d81
Andres <60366425+andtrujillo@users.noreply.github.com>
2020-02-29 17:17:01 -0600

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,16 @@
-#include "holberton.h"
+
 #include <stdio.h>
+/* main - program that prints its name followed by a new line
+ * @argc: argument count - number of arguments passed to our shell interpreter 
+ * from the command line
+ * @argv: argument vector - an array of char *s delimited by whitespace
+ */
 int main(int argc, char **argv)
 {
-	while (argc--)
-		printf("%s\n", *argv);
-	return (0);
+int i = argc - 1;
+if (i > 0)
+return (i);
+else
+printf("%s\n", argv[i]);
+return (0);
 }
