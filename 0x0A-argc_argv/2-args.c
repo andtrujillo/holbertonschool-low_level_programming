49821f0365290fe68275a1e35faf29e907b612e1
Andres <60366425+andtrujillo@users.noreply.github.com>
2020-02-29 17:31:34 -0600

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,18 @@
-#include "holberton.h"
 
+#include <stdio.h>
+/* 
+ * main - entry point of program the prints all arguments
+ * to standard output stream and the puts new line.
+ * @argc: argument count - for number of arguments
+ * @argv: argument vector of char *
+ * Return: 0 if success
+ */
 int main(int argc, char *argv[])
 {
-	while (argc--)
+	int i = argc;
+	if (i < 1)
+		return (puts(argv[1]));
+	while (i--)
 		printf("%s\n", *argv++);
 	return (0);
 }
