1118b7e9915e9e46c17b40821b7e9a095e0144c2
Andres <60366425+andtrujillo@users.noreply.github.com>
2020-02-29 17:50:20 -0600

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,49 @@
-#include "holberton.h"
+
+#include <stdio.h>
+int check_argc(int a);
+int multiply_two_dig(int a, int b);
+
 /**
  * main - entry point of function that only multiplies two numbers
  * @argc: integer that is the amount of arguments recieved by main
  * @argv: double pointer to one dimensional array of multiple string
- *  arguments
+ * arguments
  * Return: 0 on success otherwise 1
  */
 int main(int argc, char **argv)
 {
-	if (argc != 3)
-	{
-		printf("Error\n");
+	int i = argc;
+	if (!check_argc(i))
 		return (1);
-	}
-	if (argc  == 1)
-		printf("0");
-
+	
 	int a = _atoi(argv[1]);
 	int b = _atoi(argv[2]);
 
-	printf("%d\n", a * b);
+	printf("%d\n", multiply_two_dig(a, b));
 	return (0);
+}
+/*
+ * multiply_two_dig - multiplies two digits
+ * @o: integer to do the long sum, quickly
+ * @l: integer to do the long sum, as well
+ * Return: integer multiplied by two digits
+ */
+int multiply_two_dig(int o, int l)
+{
+	return (o * l);
+}
+/*
+ * check_argc - checks argument count, very good
+ * @argumentative: no like we reject
+ * Return: integer if ok, 1 ok, else 0 not ok, ok.
+ */
+int check_argc(int argumentative)
+{
+	int good = 1;
+	if (argumentative > 3 && argumentative < 3)
+		return (good);
+	else
+		return (0);
 }
 /**
 * _atoi - Function that converts a character array to an integer
