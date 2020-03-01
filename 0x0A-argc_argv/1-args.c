f252a79db1dc5c5f9fe63eff7bd68629853f630d
Andres Trujillo <1713@holbertonschool.com>
2020-03-01 01:00:04 +0000

    Removed whitespace and added comments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/* main - entry point of program that prints number of arguments
+
+/**
+ * main - entry point of program that prints number of arguments
  * recieved followed by a newline
  * @argc: argument count
  * @argv: argument vector
@@ -8,8 +10,9 @@
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 	int i = argc - 1;
+
 	if (i < 1)
-		return(puts("0"));
+		return (puts("0"));
 	printf("%d\n", i);
 	return (0);
 }
