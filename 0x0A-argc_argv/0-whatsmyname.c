5e6505748edf977ac15c82dea1677f9be7ebbf5c
Andres Trujillo <1713@holbertonschool.com>
2020-03-01 00:30:27 +0000

    Removed whitespace and included comments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,11 @@
 
 #include <stdio.h>
-/* main - program that prints its name followed by a new line
- * @argc: argument count - number of arguments passed to our shell interpreter 
+/**
+ * main - program that prints its name followed by a new line
+ * @argc: argument count - number of arguments passed to our shell interpreter
  * from the command line
  * @argv: argument vector - an array of char *s delimited by whitespace
+ * Return: 0 on success
  */
 int main(int argc, char **argv)
 {
