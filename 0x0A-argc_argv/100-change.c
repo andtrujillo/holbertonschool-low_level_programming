fc86ab2e4237f01032dad88d5399c861cb2cbd8e
Andres Trujillo <1713@example.com>
2020-03-02 00:49:25 +0000

    Removed personal atoi function

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,11 +16,11 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
-	if (_atoi(argv[1]) < 0)
+	if (atoi(argv[1]) < 0)
 		puts("0");
 	else
 	{
-		int change = _atoi(argv[1]);
+		int change = atoi(argv[1]);
 		int coins = make(change);
 
 		printf("%d\n", coins);
@@ -73,39 +73,4 @@ int make(int c)
 		count++;
 	}
 	return (count);
-}
-/**
-* _atoi - Function that converts a character array to an integer
-* @str_to: String to be converted to unsigned integer
-* Return: integer
-*/
-int _atoi(char *str_to)
-{
-	int sign_count = 1;
-	unsigned int result = 0;
-	int work = 0;
-
-	for (; *str_to != '\0'; str_to++)
-	{
-		if (!isdigit(*str_to))
-			return (-1);
-		if (*str_to == '-')
-		{
-			sign_count *= -1;
-		}
-		if (*str_to >= '0' && *str_to <= '9')
-		{
-			work = 1;
-			result = ((result * 10) + (*str_to - '0'));
-		}
-		if (*str_to < '0' || *str_to > '9')
-		{
-			if (work)
-				return (-1);
-		}
-	}
-	if (result == 0)
-		result = 0;
-
-	return (result * sign_count);
 }
