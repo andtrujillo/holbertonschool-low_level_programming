9b9c126f1705ba18ffa0c0b0be450676270d3c62
Andres Trujillo <1713@holbertonschool.com>
2020-03-03 21:57:04 +0000

    Refactored code

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,13 +13,13 @@ char *_strdup(char *str)
 char *ptr;
 unsigned int i = 0;
 
-if (str == 0)
-return (0);
+if (str == NULL)
+return (NULL);
 while (str[i] != 0)
 i++;
 i++;
 ptr = malloc(sizeof(char) * i);
-for (i = 0; str[i] != 0; i++)
+for (i = 0; str[i] != NULL; i++)
 ptr[i] = str[i];
 ptr[i] = 0;
 return (ptr);
