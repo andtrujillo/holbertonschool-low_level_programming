f946ece5b1f6a59c420ec6dbb2bd5bf68c518853
Andres Trujillo <1713@holbertonschool.com>
2020-03-03 21:51:23 +0000

    Removed _strlen function

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,54 +10,26 @@
 char *_strdup(char *str)
 {
 	char *ptr;
-	int i = 0;
-	int l = _strlen(str);
-
+	int c, i = 0;
+	
+	for (c = 0; str[c]; c++)
+				;
+		c += 1;
+	
 	if (str == NULL)
 		return (NULL);
 
-	ptr = malloc(l + 1);
+	ptr = malloc(c);
 
 	if (ptr == NULL)
 		return (NULL);
 
-	while (i <= l)
+	while (i < c)
 	{
 		ptr[i] = str[i];
 		i++;
 	}
 	ptr[i] = '\0';
-	/*_memcpy(ptr, str, _strlen(str));*/
 
 	return (ptr);
-}
-/**
- * _strlen - function that counts string length
- * @s: string
- * Return: integer type
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-
-	return (i);
-}
-/**
- * _memcpy - function that copies memory
- * @dest: destination string
- * @src: source string from which to copy from
- * @n: number of bytes to copy
- * Return: character array
- */
-char *_memcpy(char *dest, char *src, int n)
-{
-	char *d = dest;
-	const char *s = src;
-
-	while (n--)
-		*d++ = *s++;
-	return (dest);
 }
