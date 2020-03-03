0459d6930df4cf7f4995ed084cda1988c0b3d42a
Andres Trujillo <1713@holbertonschool.com>
2020-03-03 23:29:29 +0000

    Added proper checks for NULL and learned how to use free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,29 +1,47 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "holberton.h"
 /**
- * alloc_grid - function that returns a pointer to a 2 dimensional array of integers
+ * alloc_grid - function that returns a pointer
+ * to a 2 dimensional array of integers.
+ *
  * @width: columns or width of array grid
  * @height: rows or height of array gird
- * Return: integer pointer to 2-dimensional 
+ * Return: integer pointer to 2-dimensional
  */
 int **alloc_grid(int width, int height)
 {
-	int **ptr = 0;
-	int i;
+	int **ptr = NULL;
+	int i, j;
 
-	if (!width || !height)
-		return (ptr);
-	else
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr = (int **)malloc(sizeof(int *) * height);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
 	{
-		ptr = (int **) malloc (sizeof(int*) * height);
-		for (i = 0; i < height; i++)
-			ptr[i] = (int *)malloc(width* sizeof(int));
+		ptr[i] = (int *)malloc(width * sizeof(int));
+
+		if (ptr[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(ptr[j]);
+			free(ptr);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
 	}
 	return (ptr);
 }
 /**
   * _memset - Fills the first n bytes of the memory area
   * pointed to by @s with the constant byte @c.
+  *
   * @s: A pointer to the memory area to be filled.
   * @b: The character to fill the memory area with.
   * @n: The number of bytes to be filled.
