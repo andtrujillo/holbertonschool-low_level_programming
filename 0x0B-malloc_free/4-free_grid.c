3827333802c27575f4a1ef7e22197adcfe7ba340
Andres Trujillo <1713@holbertonschool.com>
2020-03-04 02:01:41 +0000

    Used linting script to fix errors in whitespace

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -5,12 +5,11 @@
  * @grid: 2-dimensional grid
  * @height: height of grid
  */
-void free_grid(int **grid, int __attribute((unused))height)
+void free_grid(int **grid, int height)
 {
 	int i;
+
 	for (i = 0; i < height; i++)
-	{
 		free(grid[i]);
-	}
 	free(grid);
 }
