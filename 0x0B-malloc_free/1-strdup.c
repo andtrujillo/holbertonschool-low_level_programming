7fe4bc9c4095e85b54909b0ed8f0dfdbb61faa99
Andres Trujillo <1713@holbertonschool.com>
2020-03-04 01:59:12 +0000

    NOMEMERR check added

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,17 +10,19 @@
 
 char *_strdup(char *str)
 {
-char *ptr;
-unsigned int i = 0;
+	char *ptr;
+	unsigned int i = 0;
 
-if (str == 0)
-return (0);
-while (str[i] != 0)
-i++;
-i++;
-ptr = malloc(sizeof(char) * i);
-for (i = 0; str[i] != 0; i++)
-ptr[i] = str[i];
-ptr[i] = 0;
-return (ptr);
+	if (str == 0)
+		return (0);
+	while (str[i] != 0)
+		i++;
+	i++;
+	ptr = malloc(sizeof(char) * i);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; str[i] != 0; i++)
+		ptr[i] = str[i];
+	ptr[i] = 0;
+	return (ptr);
 }
