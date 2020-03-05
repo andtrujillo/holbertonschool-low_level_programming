035627bbbdffb13ddfa71aacda4192ee503b7e00
Andres Trujillo <1713@holbertonschool.com>
2020-03-05 22:38:33 +0000

    Seriously, mopping bytes away from end of lines

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,11 +14,11 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	
+
 	p = malloc(sizeof(int) * (max - min + 1));
 	if (p == 0)
 		return (NULL);
-	
+
 	for (a = 0; min <= max; min++, a++)
 		p[a] = min;
 	return (p);
