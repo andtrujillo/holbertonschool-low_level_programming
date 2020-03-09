1bf948a1b21b23267d2f7cea50a325aaaab5d86a
Andres Trujillo <1713@holbertonschool.com>
2020-03-09 15:39:57 +0000

    First commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ * main - entry point to program that prints file compiled
+ * Return: zero on success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
